f3d28adaa0700d17e12c9de7ca23b3e3ec4ddf9d
Albert Ortiz <albert.ortiz90@gmail.com>
2012-11-27 21:31:01 +0100

    Commit para Adrian

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 // This file is part of Moodle - http://moodle.org/
 //
 //HOLA A TODOS!!!
+//ADRIAN ES UN PIN
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
